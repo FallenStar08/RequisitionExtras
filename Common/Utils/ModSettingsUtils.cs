abe368a50633d5630dbfa79bc11174a286d4f65e
FallenStar <23637993+FallenStar08@users.noreply.github.com>
2026-08-16 07:34:19 +0200

    Lazy util to check setting value

---
+++ Common/Utils/ModSettingsUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Terraria.ModLoader;
+using TerraStorageOverflow.Common.Systems;
+
+namespace TerraStorageOverflow.Common.Utils
+{
+    /// <summary>
+    /// This is because I'm lazy
+    /// </summary>
+    public static class ModSettingsUtils
+    {
+        private static readonly ModSettings _modSettings = ModContent.GetInstance<ModSettings>();
+
+        private static readonly Dictionary<string, Func<bool>> _boolGetters = new()
+        {
+            [nameof(ModSettings.DebugText)] = () => _modSettings.DebugText,
+
+        };
+
+        public static bool GetBoolSetting(string settingName)
+        {
+            return _boolGetters.TryGetValue(settingName, out var getter)
+                ? getter()
+                : throw new ArgumentException($"Setting '{settingName}' not found or is not a boolean.");
+        }
+    }
+}
