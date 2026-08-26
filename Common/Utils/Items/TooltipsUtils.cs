0cfb0e08fa943e16013c7a9c50d7b9e91d3c557d
FallenStar <23637993+FallenStar08@users.noreply.github.com>
2026-08-26 18:28:29 +0200

    more random utils that will totally be reused for sure

---
+++ Common/Utils/Items/TooltipsUtils.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace TerraStorageOverflow.Common.Utils.Items
+{
+    internal class TooltipsUtils
+    {
+        public static string GetRawTooltipText(Item item)
+        {
+            List<string> textPieces = [];
+
+            if (item.ModItem != null)
+            {
+                var mainTooltip = item.ModItem.GetLocalization("Tooltip");
+                if (mainTooltip != null && !string.IsNullOrEmpty(mainTooltip.Value))
+                {
+                    textPieces.Add(mainTooltip.Value);
+                }
+            }
+
+            return string.Join("\n", textPieces);
+        }
+    }
+}
